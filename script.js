9caa4208aea1b02679175fcf33fde3be5b59a013
Prathamesh S. Dahake <pratham.dahakepsd@gmail.com>
2026-06-12 18:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const button = document.querySelector('input[type="button"]');
+const dropdown = document.getElementById("colorSelect");
+
+button.addEventListener("click", () => {
+    dropdown.remove(dropdown.selectedIndex);
+});
